9d6257c4e89c3fb4009a82fd6284c7427f318c2d
Yash Garg <thisisyashgarg@gmail.com>
2023-03-05 03:08:42 +0530

    code cleanup

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,20 +1,67 @@
 import { BookData } from "../models/bookDataSchema";
-import { addDataToDB, getAllDataFromDB } from "../service/books";
 import { Request, Response } from "express";
+import { bookModel } from "../models/bookDataSchema";
+import { Error } from "mongoose";
 
-export async function addBookToDB(req: Request, res: Response) {
+export async function addBook(req: Request, res: Response) {
   try {
-    const book: BookData = await addDataToDB(req.body);
+    const book: BookData = new bookModel(req.body);
+    await book.save();
     res.status(201).json(book);
   } catch (err: unknown) {
     res.status(500).json({ error: err });
   }
 }
 
-export async function getAllBooksFromDB(req: Request, res: Response) {
+export async function getAllBooks(req: Request, res: Response) {
   try {
-    const books = getAllDataFromDB();
+    const books: BookData[] = await bookModel.find();
     res.json(books);
+  } catch (err: unknown) {
+    res.status(500).json({ error: err });
+  }
+}
+
+export async function getBookByID(req: Request, res: Response) {
+  try {
+    const book: BookData | null = await bookModel.findById(req.params.id);
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (err: unknown) {
+    res.status(500).json({ error: err });
+  }
+}
+
+export async function deleteBookByID(req: Request, res: Response) {
+  try {
+    const book: BookData | null = await bookModel.findByIdAndDelete(
+      req.params.id
+    );
+    if (book) {
+      res.json({ message: "Book deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (err: unknown) {
+    res.status(500).json({ error: err });
+  }
+}
+
+export async function updateBookByID(req: Request, res: Response) {
+  try {
+    const book: BookData | null = await bookModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
   } catch (err: unknown) {
     res.status(500).json({ error: err });
   }
--- src/routes/Books.ts
+++ src/routes/Books.ts
@@ -1,60 +1,27 @@
 import express from "express";
-import { addBookToDB, getAllBooksFromDB } from "../controllers/booksController";
-import { BookData, bookModel } from "../models/bookDataSchema";
+import {
+  addBook,
+  deleteBookByID,
+  getAllBooks,
+  getBookByID,
+  updateBookByID,
+} from "../controllers/booksController";
 
 const router = express.Router();
 
 // add a book
-router.post("/", addBookToDB);
+router.post("/", addBook);
 
 // Get all books
-router.get("/", getAllBooksFromDB);
+router.get("/", getAllBooks);
+
 // Get a single book
-router.get("/:id", async (req, res) => {
-  try {
-    const book: BookData | null = await bookModel.findById(req.params.id);
-    if (book) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (err: unknown) {
-    res.status(500).json({ error: err });
-  }
-});
+router.get("/:id", getBookByID);
 
 // delete a book
-router.delete("/:id", async (req, res) => {
-  try {
-    const book: BookData | null = await bookModel.findByIdAndDelete(
-      req.params.id
-    );
-    if (book) {
-      res.json({ message: "Book deleted successfully" });
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (err: unknown) {
-    res.status(500).json({ error: err });
-  }
-});
+router.delete("/:id", deleteBookByID);
 
 // Update a book
-router.put("/:id", async (req, res) => {
-  try {
-    const book: BookData | null = await bookModel.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    if (book) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (err: unknown) {
-    res.status(500).json({ error: err });
-  }
-});
+router.put("/:id", updateBookByID);
 
 export default router;
